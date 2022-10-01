b5bd7d31e0c680e1bf7de38085034183b09b076d
samgehrlich <gehrlism@mail.uc.edu>
2022-10-01 10:48:55 -0400

    Added tour guide variable

--- complete/src/main/java/dto/TourGuide.java
+++ complete/src/main/java/dto/TourGuide.java
@@ -2,5 +2,11 @@ package TourGuide.dto;
 
 public class TourGuide {
 
+    private String KeyLocations;
+
+    public TourGuide(String keyLocations){
+        this.KeyLocations = keyLocations;
+    }
+
 
 }
