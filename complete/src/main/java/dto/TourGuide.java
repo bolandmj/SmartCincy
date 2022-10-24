86b3a36faf845d567b9335fb8b4fe28c119d1c64
samgehrlich <gehrlism@mail.uc.edu>
2022-10-24 11:46:09 -0400

    test

---
+++ complete/src/main/java/dto/TourGuide.java
@@ -0,0 +1,13 @@
+package TourGuide.dto;
+
+public class TourGuide {
+
+    private String KeyLocations;
+
+    public TourGuide(String keyLocations){
+        this.KeyLocations = keyLocations;
+    }
+
+    public String getKeyLocations() { return KeyLocations; }
+
+}
