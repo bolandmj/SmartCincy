3b7a12a0e8f4a80bed6c9a09e463ca3aff3d7151
samgehrlich <gehrlism@mail.uc.edu>
2022-10-24 12:49:45 -0400

    added a TourGuideDAO

--- complete/src/main/java/dao/TourGuideDAO.java
+++ complete/src/main/java/dao/TourGuideDAO.java
@@ -1,4 +1,20 @@
 
 
 @Component
-public class TourGuideDAO implements
+public class TourGuideDAO implements ITourGuideDAO {
+
+    @Override
+    public TourGuide save(TourGuide tourGuide) {
+        return null;
+    }
+
+    @Override
+    public void delete(int id){
+
+    }
+
+    @Override
+    public List<TourGuide> fetchByGlobalSearch(String term) {
+        return null;
+    }
+}
